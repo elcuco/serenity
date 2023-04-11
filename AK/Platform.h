db3f7794fad1bdb2f3530fcfc4bf7671ec42488a
Cameron Youell <cameronyouell@gmail.com>
2023-04-12 02:48:04 +1000

    AK: Provide more complete definitions for AK_OS_WINDOWS MSG_NOSIGNAL is a no-op for Windows, so we can define it to 0.
    At the same *time*, none of the CLOCK_* macros are defined on
    Windows, as clock_gettime does not exist. Put AK_OS_WINDOWS in the
    same category of the BSDs for the COARSE versions of those macros.
    
    Co-authored-by: Andrew Kaster <akaster@serenityos.org>

--- AK/Platform.h
+++ AK/Platform.h
@@ -83,6 +83,9 @@
 #endif
 
 #if defined(_WIN32) || defined(_WIN64)
+#    if defined(_MSC_VER)
+#        define AK_COMPILER_MSVC
+#    endif
 #    define AK_OS_WINDOWS
 #endif
 
@@ -197,10 +200,10 @@ extern "C" {
 #endif
 
 #if defined(AK_OS_WINDOWS)
-#    define CLOCK_MONOTONIC_COARSE CLOCK_MONOTONIC
+#    define MSG_NOSIGNAL 0
 #endif
 
-#if defined(AK_OS_BSD_GENERIC) && !defined(AK_OS_FREEBSD)
+#if defined(AK_OS_BSD_GENERIC) && !defined(AK_OS_FREEBSD) || defined(AK_OS_WINDOWS)
 #    define CLOCK_MONOTONIC_COARSE CLOCK_MONOTONIC
 #    define CLOCK_REALTIME_COARSE CLOCK_REALTIME
 #endif
